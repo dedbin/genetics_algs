dc3a14b903c74ce528bac6011b922c8b5fabe401
dedbin <black13dog@yandex.ru>
2023-08-21 19:27:47 +0300

    add show_ships

--- short_path/graph_show.py
+++ short_path/graph_show.py
@@ -1,4 +1,6 @@
 from matplotlib.lines import Line2D
+from matplotlib.patches import Rectangle
+import numpy as np
 
 vertex = (
     (0, 1),
@@ -35,3 +37,28 @@ def show_graph(ax, best):
             prev = j
 
     ax.plot(vx, vy, ' ob', markersize=15)
+
+
+v_ship = np.array([0, 1, 2, 3])
+h_ship = np.array([0, 0, 0, 0])
+type_ship = [4, 3, 3, 2, 2, 2, 1, 1, 1, 1]
+colors = ['g', 'b', 'm', 'y']
+
+
+def show_ships(ax, best, pole_size):
+    rect = Rectangle((0, 0), pole_size + 1, pole_size + 1, fill=None, edgecolor='r')
+
+    t_n = 0
+    for i in range(0, len(best), 3):
+        x = best[i]
+        y = best[i + 1]
+        r = best[i + 2]
+        t = type_ship[t_n]
+        t_n += 1
+
+        if r == 1:
+            ax.plot(v_ship[:t] + x, h_ship[:t] + y, ' sb', markersize=18, alpha=0.8, markerfacecolor=colors[t - 1])
+        else:
+            ax.plot(h_ship[:t] + x, v_ship[:t] + y, ' sb', markersize=18, alpha=0.8, markerfacecolor=colors[t - 1])
+
+    ax.add_patch(rect)
